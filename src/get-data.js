e44f3067110d0090d1618f30a50b21d78bb434f8
scoopd0303 <scott.russell.mcdonald@gmail.com>
2021-12-14 13:47:10 -0600

    quiz accumulates correct answers accurately and now displays results at end of quiz.

--- src/get-data.js
+++ src/get-data.js
@@ -1,6 +1,87 @@
 
 export function GetData() {
     return (
-        { "response_code": 0, "results": [{ "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "In any programming language, what is the most common way to iterate through an array?", "correct_answer": "&#039;For&#039; loops", "incorrect_answers": ["&#039;If&#039; Statements", "&#039;Do-while&#039; loops", "&#039;While&#039; loops"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "When Gmail first launched, how much storage did it provide for your email?", "correct_answer": "1GB", "incorrect_answers": ["512MB", "5GB", "Unlimited"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What does GHz stand for?", "correct_answer": "Gigahertz", "incorrect_answers": ["Gigahotz", "Gigahetz", "Gigahatz"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "HTML is what type of language?", "correct_answer": "Markup Language", "incorrect_answers": ["Macro Language", "Programming Language", "Scripting Language"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What is the most preferred image format used for logos in the Wikimedia database?", "correct_answer": ".svg", "incorrect_answers": [".png", ".jpeg", ".gif"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What is the domain name for the country Tuvalu?", "correct_answer": ".tv", "incorrect_answers": [".tu", ".tt", ".tl"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "On Twitter, what was the original character limit for a Tweet?", "correct_answer": "140", "incorrect_answers": ["120", "160", "100"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What does LTS stand for in the software market?", "correct_answer": "Long Term Support", "incorrect_answers": ["Long Taco Service", "Ludicrous Transfer Speed", "Ludicrous Turbo Speed"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "How many values can a single byte represent?", "correct_answer": "256", "incorrect_answers": ["8", "1", "1024"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "In computing, what does LAN stand for?", "correct_answer": "Local Area Network", "incorrect_answers": ["Long Antenna Node", "Light Access Node", "Land Address Navigation"] }] }["results"]
+        { "response_code": 0, "results": [
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "In any programming language, what is the most common way to iterate through an array?", 
+                "correct_answer": "For loops", 
+                "incorrect_answers": ["If Statements", "Do-while loops", "While loops"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "When Gmail first launched, how much storage did it provide for your email?", 
+                "correct_answer": "1GB", 
+                "incorrect_answers": ["512MB", "5GB", "Unlimited"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "What does GHz stand for?", 
+                "correct_answer": "Gigahertz", 
+                "incorrect_answers": ["Gigahotz", "Gigahetz", "Gigahatz"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "HTML is what type of language?", 
+                "correct_answer": "Markup Language", 
+                "incorrect_answers": ["Macro Language", "Programming Language", "Scripting Language"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "What is the most preferred image format used for logos in the Wikimedia database?", 
+                "correct_answer": ".svg", 
+                "incorrect_answers": [".png", ".jpeg", ".gif"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "What is the domain name for the country Tuvalu?", 
+                "correct_answer": ".tv", 
+                "incorrect_answers": [".tu", ".tt", ".tl"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "On Twitter, what was the original character limit for a Tweet?", 
+                "correct_answer": "140", 
+                "incorrect_answers": ["120", "160", "100"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "What does LTS stand for in the software market?", 
+                "correct_answer": "Long Term Support", 
+                "incorrect_answers": ["Long Taco Service", "Ludicrous Transfer Speed", "Ludicrous Turbo Speed"] 
+            },
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "How many values can a single byte represent?", 
+                "correct_answer": "256", 
+                "incorrect_answers": ["8", "1", "1024"] 
+            }, 
+            { 
+                "category": "Science: Computers", 
+                "type": "multiple", 
+                "difficulty": "easy", 
+                "question": "In computing, what does LAN stand for?", 
+                "correct_answer": "Local Area Network", 
+                "incorrect_answers": ["Long Antenna Node", "Light Access Node", "Land Address Navigation"] 
+            }
+        ] }["results"]
     )
 }
